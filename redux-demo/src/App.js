5660b719722750916b8cc2c902cb85286551876a
David ChromeOS <me@davidsalomon.dev>
2021-12-01 23:14:16 -0600

    ⚡️ connect redux with React

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import { Provider } from "react-redux";
 import CakeContainer from "./components/CakeContainer";
+import store from "./redux/store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
 
 function App() {
 	return (
-		<div className="App">
-			<CakeContainer />
-		</div>
+		<Provider store={store}>
+			<div className="App">
+				<CakeContainer />
+				<HooksCakeContainer />
+				<IceCreamContainer />
+			</div>
+		</Provider>
 	);
 }
 
